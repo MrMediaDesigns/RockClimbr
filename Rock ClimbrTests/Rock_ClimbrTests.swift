da03d0af0ec6b8d3ac4ef7a5aeccb3f190a884cd
Collin Scott <mrmediadesigns@gmail.com>
2016-05-02 11:58:10 -0400

    Initial Commit

---
+++ Rock ClimbrTests/Rock_ClimbrTests.swift
@@ -0,0 +1,36 @@
+//
+//  Rock_ClimbrTests.swift
+//  Rock ClimbrTests
+//
+//  Created by Collin Scott on 5/2/16.
+//  Copyright © 2016 Rocket Scientist. All rights reserved.
+//
+
+import XCTest
+@testable import Rock_Climbr
+
+class Rock_ClimbrTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
